455c723bf7e75e1b88bae6fe52a6406e5584fcb4
gmkvaal <guttorm.kvaal@gmail.com>
2017-11-27 16:50:10 +0100

    Some formating

--- network.go
+++ network.go
@@ -6,43 +6,18 @@ import (
 	"log"
 )
 
-// NOTE: Maybe not so smart with all these global variables?
-// Perhaps put them in a struct and create an object and
-// then use overwriting through pointers???
-
-// Variables to hold the activation(s) for one node,
-// nodes on one layer, and all layers, respectively
-var zNode float64
-var zLayer []float64
-var zAllLayers [][]float64
-
-// Variables to hold the sigmoid(s) for one node,
-// nodes on a new layer, input layer, and every
-// layer, respectively.
-var activationNode float64
-var activationNewLayer []float64
-var activationLayer []float64
-var activationAllLayers [][]float64
-
-var delta [][]float64
-
-
-
-var sigPrimeLayer []float64
-
 
 // networkFormat contains the
 // fields sizes, biases, and weights
 type networkFormat struct {
-	sizes   []int
-	biases  [][]float64
-	weights [][][]float64
-	delta [][]float64
-	nablaW [][][]float64
-	nablaB [][]float64
-	z [][]float64
+	sizes       []int
+	biases      [][]float64
+	weights     [][][]float64
+	delta       [][]float64
+	nablaW      [][][]float64
+	nablaB      [][]float64
+	z           [][]float64
 	activations [][]float64
-
 }
 
 // initNetwork initiates the weights
@@ -56,8 +31,6 @@ func (nf *networkFormat) initNetwork() {
 	nf.z = nf.squareMatrix(zeroFunc())
 	nf.activations = nf.squareMatrixFull(zeroFunc())
 }
-
-
 
 // squareMatrix creates a square matrix with entries from input function
 func (nf networkFormat) squareMatrix(input func(int) float64) [][]float64 {
@@ -113,7 +86,7 @@ func (nf networkFormat) cubicMatrix(input func(int) float64) [][][]float64 {
 func (nf networkFormat) backProp(x []float64, y [10]float64) ([][]float64, [][][]float64) {
 
 	// Initiating the gradient matrices
-	l := len(nf.sizes) - 1// last entry "layer-vise"
+	l := len(nf.sizes) - 1 // last entry "layer-vise"
 
 	// Clearing / preparing the slices
 	nf.activations[0] = x
@@ -136,10 +109,10 @@ func (nf networkFormat) backProp(x []float64, y [10]float64) ([][]float64, [][][
 	nf.nablaW[l-1] = vectorMatrixProduct(nf.nablaW[l-1], nf.delta[l-1], nf.activations[l-1])
 
 	// Backpropagating the error
-	for k := 2; k < l + 1; k++ {
+	for k := 2; k < l+1; k++ {
 		for j := 0; j < nf.sizes[l+1-k]; j++ {
 			for i := 0; i < nf.sizes[l+2-k]; i++ {
-				nf.delta[l-k][j] += nf.weights[l+1-k][i][j]*nf.delta[l+1-k][i] * sigmoidPrime(nf.z[l+1-k][i])
+				nf.delta[l-k][j] += nf.weights[l+1-k][i][j] * nf.delta[l+1-k][i] * sigmoidPrime(nf.z[l+1-k][i])
 			}
 
 			nf.nablaB[l-k][j] = nf.delta[l-k][j]
@@ -150,16 +123,11 @@ func (nf networkFormat) backProp(x []float64, y [10]float64) ([][]float64, [][][
 		}
 
 	}
-
+	
 	return nf.nablaB, nf.nablaW
-
 }
 
-
-
-
 func main() {
-
 
 	// Load files of type GoMNIST which is actually []byte, where the byte value
 	train, _, err := GoMNIST.Load("/home/guttorm/xal/go/src/github.com/petar/GoMNIST/data")
@@ -186,4 +154,3 @@ func main() {
 
 	//fmt.Println(nablaW)
 }
-
