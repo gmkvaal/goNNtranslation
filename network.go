42ff097c5964e6606b376aae8eb8f59424bcf3e9
gmkvaal <guttorm.kvaal@gmail.com>
2017-12-18 14:52:53 +0100

    no longer making copies of gradients, just adding on the objects

--- network.go
+++ network.go
@@ -66,42 +66,35 @@ func (nf *NetworkFormat) outputError(y []float64, l int) {
 	}
 }
 
-func (nf *NetworkFormat) outputGradients(nablaW [][][]float64, nablaB [][]float64, l int) ([][]float64, []float64) {
+func (nf *NetworkFormat) outputGradients(l int)  {
 	for j := 0; j < nf.Sizes[l]; j++ {
-		nablaB[l-1][j] += nf.delta[l-1][j]
+		nf.nablaB[l-1][j] += nf.delta[l-1][j]
 		for i := 0; i < nf.Sizes[l-1]; i++ {
-			nablaW[l-1][j][i] += nf.delta[l-1][j]*nf.activations[l-1][i]
+			nf.nablaW[l-1][j][i] += nf.delta[l-1][j]*nf.activations[l-1][i]
 		}
 	}
-
-	return nablaW[l-1], nablaB[l-1]
 }
 
 // backPropError backpropagates the error through the hidden layers
-func (nf *NetworkFormat) backPropError(nablaW [][][]float64, nablaB [][]float64, l int) ([][][]float64, [][]float64){
+func (nf *NetworkFormat) backPropError(l int) {
 	for k := 2; k < l+1; k++ {
 		for j := 0; j < nf.Sizes[l+1-k]; j++ {
 			nf.delta[l-k][j] = 0
 			for i := 0; i < nf.Sizes[l+2-k]; i++ {
 				nf.delta[l-k][j] += nf.weights[l+1-k][i][j] * nf.delta[l+1-k][i] * sigmoidPrime(nf.z[l-k][j])
 			}
-			nablaB[l-k][j] +=  nf.delta[l-k][j]
+			nf.nablaB[l-k][j] +=  nf.delta[l-k][j]
 
 			for i := 0; i < nf.Sizes[l-k]; i++ {
-				nablaW[l-k][j][i] += nf.delta[l-k][j] * nf.activations[l-k][i]
+				nf.nablaW[l-k][j][i] += nf.delta[l-k][j] * nf.activations[l-k][i]
 			}
 		}
 	}
-
-	return nablaW, nablaB
 }
 
 // backProp performs one iteration of the backpropagation algorithm
 // for input x and training output y (one batch in a mini batch)
-func (nf *NetworkFormat) backPropAlgorithm(x, y []float64) ([][][]float64, [][]float64){
-
-	nablaW := nf.cubicMatrix(zeroFunc())
-	nablaB := nf.squareMatrix(zeroFunc())
+func (nf *NetworkFormat) backPropAlgorithm(x, y []float64) {
 
 	l := len(nf.Sizes) - 1 // last entry "layer-vise"
 
@@ -112,32 +105,26 @@ func (nf *NetworkFormat) backPropAlgorithm(x, y []float64) ([][][]float64, [][]f
 	nf.outputError(y, l)
 
 	// 3. Gradients at the output layer
-	nablaW[l-1], nablaB[l-1] = nf.outputGradients(nablaW, nablaB, l)
+	nf.outputGradients(l)
 
 	// 4. Backpropagating the error
-	nablaW, nablaB = nf.backPropError(nablaW, nablaB, l)
-
-	return nablaW, nablaB
+	nf.backPropError(l)
 }
 
 // updateMiniBatches runs the stochastic gradient descent
 // algorithm for a set of mini batches (e.g one epoch)
 func (nf *NetworkFormat) updateMiniBatches() {
-	deltaNablaW := nf.cubicMatrix(zeroFunc())
-	deltaNablaB := nf.squareMatrix(zeroFunc())
 
 	for i := range nf.data.miniBatches {
-		nablaW := nf.cubicMatrix(zeroFunc())
-		nablaB := nf.squareMatrix(zeroFunc())
+		nf.nablaW = nf.cubicMatrix(zeroFunc())
+		nf.nablaB = nf.squareMatrix(zeroFunc())
 
 		for _, dataSet := range nf.data.miniBatches[i] {
-			deltaNablaW, deltaNablaB = nf.backPropAlgorithm(dataSet[0], dataSet[1])
-			nablaW = nf.updateNablaW(deltaNablaW, nablaW)
-			nablaB = nf.updateNablaB(deltaNablaB, nablaB)
+			nf.backPropAlgorithm(dataSet[0], dataSet[1])
 		}
 
-		nf.updateWeights(nablaW)
-		nf.updateBiases(nablaB)
+		nf.updateWeights()
+		nf.updateBiases()
 	}
 }
 
@@ -165,22 +152,22 @@ func (nf *NetworkFormat) updateNablaW(deltaNablaW[][][]float64,	nablaW [][][]flo
 }
 
 // updateWeights updates the weight matrix following a mini batch
-func (nf *NetworkFormat) updateWeights(nablaW [][][]float64) {
+func (nf *NetworkFormat) updateWeights() {
 	for k := 0; k < len(nf.Sizes) - 1; k++ {
 		for j := 0; j < nf.Sizes[k+1]; j++ {
 			for i := 0; i < nf.Sizes[k]; i++ {
 				nf.weights[k][j][i] = (1 - nf.hp.eta*(nf.hp.lambda/nf.data.n))*nf.weights[k][j][i] -
-					nf.hp.eta/nf.data.miniBatchSize * nablaW[k][j][i]
+					nf.hp.eta/nf.data.miniBatchSize * nf.nablaW[k][j][i]
 			}
 		}
 	}
 }
 
 // updateBiases updates the bias matrix following a mini batch
-func (nf *NetworkFormat) updateBiases(nablaB [][]float64) {
+func (nf *NetworkFormat) updateBiases() {
 	for k := 0; k < len(nf.Sizes) - 1; k++ {
 		for j := 0; j < nf.Sizes[k+1]; j++ {
-			nf.biases[k][j] = nf.biases[k][j] - nf.hp.eta/nf.data.miniBatchSize*nablaB[k][j]
+			nf.biases[k][j] = nf.biases[k][j] - nf.hp.eta/nf.data.miniBatchSize*nf.nablaB[k][j]
 		}
 	}
 }
